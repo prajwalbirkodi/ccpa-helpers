174938ab10026dfac15035ca9e02b8df042548df
prajwalbirkodi <36972991+prajwalbirkodi@users.noreply.github.com>
2024-08-03 18:11:14 -0500

    Add files via upload
    Initial load

---
+++ app.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+import streamlit as st
+from ccpa_helpers import CCPAAnonymizer
+import pandas as pd
+
+
+# Define the Streamlit app
+def main():
+
+    st.title("CCPA Anonymizer")
+
+    st.info("Before anonymizing the dataset, please make sure Docker is running on your system.")
+
+    st.sidebar.header("Configuration")
+    project_name = st.sidebar.text_input("Project Name", value="ccpa-anonymized")
+    transforms_config = st.sidebar.text_input("Transforms Config", value="src/config/transforms_config.yaml")
+    synthetics_config = st.sidebar.text_input("Synthetics Config", value="src/config/synthetics_config.yaml")
+    endpoint = st.sidebar.text_input("Endpoint", value="https://api.gretel.cloud")
+
+    st.sidebar.header("Dataset")
+    uploaded_files = st.sidebar.file_uploader("Upload Dataset", type=["csv", "xlsx"], accept_multiple_files=True)
+
+    st.sidebar.header("Options")
+    overwrite = st.sidebar.checkbox("Overwrite", value=False)
+
+    transform_locally = st.checkbox("Transform Locally")
+    transform_in_cloud = st.checkbox("Transform in Cloud", value=not transform_locally, key="transform_in_cloud_key", disabled=transform_locally)
+    synthesize_locally = st.checkbox("Synthesize Locally")
+    synthesize_in_cloud = st.checkbox("Synthesize in Cloud", value=not synthesize_locally, key="synthesize_in_cloud_key", disabled=synthesize_locally)
+
+    if transform_locally:
+        transform_in_cloud = False
+    elif transform_in_cloud:
+        transform_locally = False
+    
+    if synthesize_locally:
+        synthesize_in_cloud = False
+    elif synthesize_in_cloud:
+        synthesize_locally = False
+
+    if st.button("Anonymize"):
+        if uploaded_files is not None:
+            for uploaded_file in uploaded_files:
+                # Save the uploaded file to a temporary directory
+                with open(uploaded_file.name, "wb") as f:
+                    f.write(uploaded_file.getvalue())
+                
+                # Initialize the CCPAAnonymizer object
+                anonymizer = CCPAAnonymizer(
+                    project_name=project_name,
+                    transforms_config=transforms_config,
+                    synthetics_config=synthetics_config,
+                    endpoint=endpoint,
+                    overwrite=overwrite
+                )
+
+                with st.spinner("Anonymization in progress..."):
+
+                    anonymizer.anonymize(
+                        dataset_path=uploaded_file.name,
+                        transform_locally=transform_locally,
+                        transform_in_cloud=transform_in_cloud,
+                        synthesize_locally=synthesize_locally,
+                        synthesize_in_cloud=synthesize_in_cloud
+                )
+                
+                st.success("Anonymization Complete")
+
+                if transform_locally or transform_in_cloud:
+                    st.subheader("First 10 records from the transformed file:")
+                    transformed_df = pd.read_csv(anonymizer.deidentified_path)
+                    st.write(transformed_df.head(10))
+
+                    # Display the first 10 records from the synthesized file if applicable
+                if synthesize_locally or synthesize_in_cloud:
+                    st.subheader("First 10 records from the synthesized file:")
+                    synthesized_df = pd.read_csv(anonymizer.anonymized_path)
+                    st.write(synthesized_df.head(10))
+        else:
+            st.error("Please upload a dataset file.")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+local_path = pathlib.Path(__file__).parent
+install_requires = (local_path / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="ccpa-helpers",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    package_dir={"": "src"},
+    install_requires=install_requires,
+    python_requires=">=3.7",
+    packages=find_packages("src"),
+    package_data={"": ["*config/*.yaml"]},  # Ensure YAML files in config directory are included
+    include_package_data=True,
+    description="Synthetic Data for CCPA",
+    url="https://github.com/gretelai/ccpa-helpers",
+    license="https://gretel.ai/license/source-available-license",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free To Use But Restricted",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ccpa-synthetics=app:main',
+        ],
+    },
+)
